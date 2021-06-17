8408d331114e123b5418e8c527c94e7a03cb0d29
Dan Skrypnik <forte201253@gmail.com>
2021-06-17 13:27:46 +0300

    minor change

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ const seedData = async () => {
 
   if (devices.length === 0) {
     const devices = [
-      new DeviceModel({ name: 'IPhone XR', kind: 'phone', price: 600 }),
+      new DeviceModel({ name: 'IPhone XS', kind: 'phone', price: 600 }),
       new DeviceModel({ name: 'Fifine k770', kind: 'microphone', price: 200 }),
       new DeviceModel({ name: 'HH Keyboard', kind: 'keyboard', price: 200 }),
     ];
