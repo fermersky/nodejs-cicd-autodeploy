8be35a01dddba8062048162a720a849045561a19
Dan Skrypnik <forte201253@gmail.com>
2021-06-19 23:09:58 +0300

    minor change

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ const seedData = async () => {
 
   if (devices.length === 0) {
     const devices = [
-      new DeviceModel({ name: 'IPhone XS', kind: 'phone', price: 600 }),
+      new DeviceModel({ name: 'IPhone XR', kind: 'phone', price: 600 }),
       new DeviceModel({ name: 'Fifine k770', kind: 'microphone', price: 200 }),
       new DeviceModel({ name: 'HH Keyboard', kind: 'keyboard', price: 200 }),
     ];
@@ -59,7 +59,7 @@ const seedData = async () => {
   });
 
   app.get('/', async (req, res) => {
-    res.end('please try /cicd/sessions or /cicd/devices')
+    res.end('Please try /cicd/sessions or /cicd/devices')
   });
 
   app.get('/sessions', async (req, res) => {
